141bc15b18699ebd5421310b8ec1e89b5c653528
Henrique Soares <henrique@trabalho.(none)>
2011-06-15 11:16:52 -0300

    Template function - very simple

--- guiga/guiga.js
+++ guiga/guiga.js
@@ -1,7 +1,23 @@
-$.fn.extend({
-  serializeGrid: function() {
-    $.each($('tbody tr', this), function() {
-      console.log($(this));
-    });
-  }
-});
+// $.fn.extend({
+//   serializeGrid: function() {
+//     $.each($('tbody tr', this), function() {
+//       console.log($(this));
+//     });
+//   }
+// });
+
+// Forked from https://gist.github.com/860240
+function tmpl(str, data) {
+  var value = "var out = ''; out+=" + "'" +
+
+  str.replace(/[\r\t\n]/g, " ")
+    .replace(/'(?=[^%]*%>)/g,"\t")
+    .split("'").join("\\'")
+    .split("\t").join("'")
+    .replace(/<%=(.+?)%>/g, "'; out += $1; out += '")
+    .split("<%").join("';")
+    .split("%>").join("out+='")
+    + "'; return out;";
+
+  return new Function("data", value);
+}
